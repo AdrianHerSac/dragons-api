b8c85aa4120f7d18c6c53897b5bb85c363ba2758
AdrianHerSac <adrianherrero2016@gmail.com>
2025-11-21 20:28:08 +0100

    Correcciones y creacion dto dragon

--- src/main/java/dev/adrian/dragonsapi/dragons/service/DragonServiceImpl.java
+++ src/main/java/dev/adrian/dragonsapi/dragons/service/DragonServiceImpl.java
@@ -1,25 +1,19 @@
 package dev.adrian.dragonsapi.dragons.service;
 
-import dev.adrian.dragonsapi.dragons.model.Category;
 import dev.adrian.dragonsapi.dragons.model.Dragon;
 import dev.adrian.dragonsapi.dragons.repository.DragonRepository;
-import jakarta.persistence.criteria.Join;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.lang.annotation.Annotation;
 import java.util.Optional;
 
 /**
  *
  */
 @Service
-@CacheConfig(cacheNames = {"dragons"})
 public class DragonServiceImpl implements DragonService {
 
     private final Logger log = LoggerFactory.getLogger(DragonServiceImpl.class);
@@ -30,44 +24,13 @@ public class DragonServiceImpl implements DragonService {
         this.dragonRepository = dragonRepository;
     }
 
+
     @Override
     public Page<Dragon> findAll(Optional<String> name,
                                 Optional<Double> maxSpeed,
                                 Optional<String> category,
                                 Pageable pageable) {
 
-        Specification<Dragon> specName = (root, query, criteriaBuilder) ->
-                name.map(n -> criteriaBuilder.like(
-                                criteriaBuilder.lower(root.get("name")),
-                                "%" + n.toLowerCase() + "%"))
-                        .orElse(criteriaBuilder.conjunction());
-
-        Specification<Dragon> specMaxSpeed = (root, query, criteriaBuilder) ->
-                maxSpeed.map(s -> criteriaBuilder.lessThanOrEqualTo(root.get("speed"), s))
-                        .orElse(criteriaBuilder.conjunction());
-
-        Specification<Dragon> specCategory = (root, query, criteriaBuilder) ->
-                category.map(c -> {
-                    Join<Dragon, Category> categoryJoin = root.join("category");
-                    return criteriaBuilder.like(
-                            criteriaBuilder.lower(categoryJoin.get("name")),
-                            "%" + c.toLowerCase() + "%");
-                }).orElse(criteriaBuilder.conjunction());
-
-        Specification<Dragon> finalSpec = Specification.where(specName)
-                .and(specMaxSpeed)
-                .and(specCategory);
-
-        return dragonRepository.findAll(finalSpec, pageable);
-    }
-
-    @Override
-    public String value() {
-        return "";
-    }
-
-    @Override
-    public Class<? extends Annotation> annotationType() {
         return null;
     }
 }
